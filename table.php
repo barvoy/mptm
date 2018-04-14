81cdd42814daa5e0bf4bf73c13e18a91b8193f17
Wojciech Adam Koszek <wojciech@koszek.com>
2018-04-13 18:08:28 -0700

    fix table generation.

--- table.php
+++ table.php
@@ -39,12 +39,28 @@ function row_add($num, $name, $val) {
 	return $ostr;
 }
 
+function tm_str_usb_amt($val) {
+	return sprintf("$%2.2f", $val);
+}
+
 function tm_str_new_mem_ini_fee() {
 	return "Toastmasters International: new member processing fee";
 }
 
 function tm_str_monthly($how_many_months, $how_much_per_month) {
-	return "Toastmasters International: $how_many_months * $$how_much_per_month";
+	return "Toastmasters International: $how_many_months * " . tm_str_usb_amt($how_much_per_month);
+}
+
+function tm_str_ca_tax($rate) {
+	$pc_rate = $rate * 100;
+	$pc_rate .= "%";
+	return "CA Tax of $pc_rate";
+}
+
+function tm_str_paypal($rate) {
+	$pc_rate = $rate * 100;
+	$pc_rate .= "%";
+	return "PayPal payment processing rate of $pc_rate";
 }
 
 function tm_str_total() {
@@ -68,9 +84,37 @@ function make_order_items_array($cfg, $order) {
 	}
 
 	// b. we charge TMI's per-month fee
-	$mo_cost = $club_cfg->{'fees'}->{'tmi_monthly'};
-	$amt = $order_memb_start_mo * $mo_cost;
-	array_push($item_all, [ $item_num, tm_str_monthly($order_memb_start_mo, $mo_cost), $amt ]);
+	$tmi_mo_cost = $club_cfg->{'fees'}->{'tmi_monthly'};
+	$amt = $order_memb_start_mo * $tmi_mo_cost;
+	array_push($item_all, [ $item_num, tm_str_monthly($order_memb_start_mo, $tmi_mo_cost), $amt ]);
+	$item_num += 1;
+
+	// c. we charge club's per-month fee
+	$club_mo_cost = $club_cfg->{'fees'}->{'club_monthly'};
+	$amt = $order_memb_start_mo * $club_mo_cost;
+	array_push($item_all, [ $item_num, tm_str_monthly($order_memb_start_mo, $club_mo_cost), $amt ]);
+	$item_num += 1;
+
+	// add itemized charges here
+
+	// x. charge CA tax
+	$ca_tax_rate = $club_cfg->{'fees'}->{'ca_tax_rate_mul'};
+	$total_so_far = 0;
+	foreach ($item_all as $arr_idx => $arr) {
+		$total_so_far += $arr[2];
+	}
+	$amt = $total_so_far * ($ca_tax_rate);
+	array_push($item_all, [ $item_num, tm_str_ca_tax($ca_tax_rate), $amt ]);
+	$item_num += 1;
+
+	// y. charge PayPal rate
+	$paypal_rate = $club_cfg->{'fees'}->{'paypay_rate_mul'};
+	$total_so_far = 0;
+	foreach ($item_all as $arr_idx => $arr) {
+		$total_so_far += $arr[2];
+	}
+	$amt = $total_so_far * ($paypal_rate);
+	array_push($item_all, [ $item_num, tm_str_paypal($paypal_rate), $amt ]);
 	$item_num += 1;
 
 	// z. add a total count
@@ -78,7 +122,7 @@ function make_order_items_array($cfg, $order) {
 	foreach ($item_all as $arr_idx => $arr) {
 		$total_amt += $arr[2];	// amount
 	}
-	array_push($item_all, [ '<b>=</b>', '<b>' . tm_str_total() . '</b>', '<b>' . $total_amt ]);
+	array_push($item_all, [ '<b>=</b>', '<b>' . tm_str_total() . '</b>', $total_amt ]);
 	$item_num += 1;
 
 	return $item_all;
