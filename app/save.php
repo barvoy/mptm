89c7cf24c9e6ef5e0f6d4a4f897e24f0018847f6
Wojciech Adam Koszek <wojciech@koszek.com>
2018-04-20 00:04:44 -0700

    Remove unnecessary junk

--- app/save.php
+++ app/save.php
@@ -10,12 +10,6 @@ $sub_data = array(
 	"post" => $_POST
 	,
 	"get" => $_GET
-	,
-	"server" => $_SERVER
-	,
-	"cookie" => $_COOKIE
-	,
-	"request" => $_REQUEST
 );
 
 $j_str = json_encode($sub_data, JSON_PRETTY_PRINT);
