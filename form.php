1b9739b7819d0539b7252ed1576b6a4e042e4cd3
Wojciech Adam Koszek <wojciech@koszek.com>
2018-04-19 18:56:36 -0700

    Refactor to 1 project.

--- form.php
+++ form.php
@@ -2,251 +2,291 @@
 error_reporting(E_STRICT);
 
 include_once('lib.php');
-	state_init();
-	state_trans_from_to("init", "form");
+//	state_init();
+//	state_trans_from_to("init", "form");
 ?>
 
-<style>
-body { font-family: sans-serif; }
-input.submit { background-color: lightyellow ; width: 10%; font-size: 100%; }
-span.req { color: red; }
-input { background-color: lightyellow; }
-legend { yellow; border: dotted 1px lightgray ;}
-.req_mark { background-color: lightyellow; }
-</style>
+<html>
+<head>
+		<link rel="stylesheet" href="tm.css">
+</head>
 
-<h4>We're happy to see you joining Menlo Park Toastmasters Club!<h4>
-<h1>Application & Payment Information</h1>
+<body>
 
-
-<style>
-.left { float: left; margin-right: 2%; }
-.line { margin-bottom: 1%; }
-.none { float: none; }
-</style>
-
-<span class="req">*</span> fields are required
-<br />
-
-<form action="save.php" method="post">
-  <fieldset>
-    <legend>Personal information</legend>
-
-    <div class="form">
-      <div class="line">
-        <div class="left">
-          <label for="firstname">First name:</label>
-          <span class="req">*</span>
-          <br />
-          <input type="text" id="firstname" name="firstname" placeholder="..." required>
-        </div>
-        <div class="left">
-          <label for="middlename">Middle name:</label>
-          <br />
-          <input type="text" id="middlename" name="middlename" placeholder="...">
-        </div>
-        <div class="none">
-          <label for="lastname">Last name/Surname:</label>
-          <span class="req">*</span>
-          <br />
-          <input type="text" id="lastname" name="lastname" placeholder="..." required>
-        </div>
-      </div>
-
-      <div class="line">
-        <div class="none">
-          <label for="gender">Gender:</label>
-          <br />
-          <input type="radio" name="gender" value="male" />Male
-          <input type="radio" name="gender" value="female" />Female
-          <input type="radio" name="gender" value="other" checked />Other
-        </div>
-      </div>
-
-      <div class="line">
-
-        <div class="lineitem" style="width: 100%">
-          <label for="address1">Address 1:</label>
-          <span class="req">*</span>
-          <br />
-          <input required type="text" id="address1" name="address1" placeholder="Address 1..." width="100%" />
-        </div>
-
-	<br />
-        <div class="lineitem" style="width: 100%">
-          <label for="address2">Address 2:</label>
-          <br />
-          <input type="text" id="address2" name="address2" placeholder="Address 2..." width="100%" />
-          <br />
-        </div>
-
-	<br />
-        <div class="lineitem">
-          <label for="zipcode">ZIP code:</label>
-          <span class="req">*</span>
-          <br />
-          <input required type="text" id="zipcode" name="zipcode" placeholder="xxxxx" size=5>
-          <br />
-        </div>
-
-	<br />
-        <div class="lineitem">
-          <label for="cityname">City:</label>
-          <span class="req">*</span>
-          <br />
-          <input required type="text" id="city" name="city" placeholder="Your city name..." size=100>
-          <br />
-        </div>
-
-	<br />
-        <div class="lineitem">
-          <label for="state">State:</label>
-          <span class="req">*</span>
-          <br />
-          <input required type="text" id="state" name="state" placeholder="Your state..." size=100>
-          <br />
-        </div>
-
-
-	<br />
-        <div class="lineitem">
-          <label for="email">E-mail:</label>
-          <span class="req">*</span>
-          <br />
-          <input required type="text" id="email" name="email" placeholder="Your e-mail..." size=100>
-          <br />
-        </div>
-
-
-
-      </div>
-    </div>
-  </fieldset>
-<br />
-
-<!--
-<hr />
-<select>
-<option>English</option>
-<option>German</option>
-<option>Portugese</option>
-<option>French</option>
-<option>Spanish</option>
-<option>简体中文</option>
-<option>繁體中文</option>
-</select>
-<hr />
--->
-
-
-<fieldset>
-	<legend>
-		<span class="req">*</span>
-		Membership Type
-	</legend>
-
-	<br />
-	<input type="radio" name="membership_type" class="membership_type" value="new" checked>New</input><br />
-	<input type="radio" name="membership_type" class="membership_type" value="reinstated">Reinstated (break in membership)</input><br />
-	<input type="radio" name="membership_type" class="membership_type" value="renewing">Renewing (no break in membership)</input><br />
-	<input type="radio" name="membership_type" class="membership_type" value="dual">Dual</input><br />
-	<input type="radio" name="membership_type" class="membership_type" value="transfer">Transfer</input><br />
-	<br />
-
-	<span id="membership_type_transfer_data" hidden>
-		<h3>Transferring: provide details here</h3>
-		Previous club name:
-		<span class="req">*</span>
-		<input type="text" name="trans_prev_club_name" placeholder="Previous club name" />
-		<br />
-
-		Previous club number:
-		<span class="req">*</span>
-		<input type="text" name="trans_prev_club_num" placeholder="Previous club number" />
-		<br />
-
-		Member number:
-		<span class="req">*</span>
-		<input type="text" name="trans_memb_number" placeholder="Member number" />
-		<br />
-	</span>
-</fieldset>
-<br />
-
-<fieldset hidden>
-	<legend>Club information</legend>
-
-	<label for="club_city">Club city:</label>
-	<input type="text" id="club_city" name="club_city" value="Menlo Park" size=30 />
-	<br />
-
-	<label for="club_name">Club name:</label>
-	<input type="text" id="club_name" name="club_name" value="Menlo Park Toastmasters" />
-
-	<label for="club_number">Club number:</label>
-	<input type="text" id="club_number" name="club_number" value="1372" />
-</fieldset>
-
-<fieldset>
-	<legend>
-		<span class="req">*</span>
-		A Toastmaster's Promise
-	</legend>
-
-	As a member of Toastmasters International and my club, I promise:
+	<h1>Join Menlo Park Toastmasters!</h1>
+	<!--
 	<p>
-		<li>To attend club meetings regularly</li>
-		<li>To prepare all of my projects to the best of my ability, basing them on the Toastmasters education program</li>
-		<li>To prepare for and fulfill meeting assignments</li>
-		<li>To provide fellow members with helpful, constructive evaluations</li>
-		<li>To help the club maintain the positive, friendly environment necessary for all members to learn and grow</li>
-		<li>To serve my club as an officer when called upon to do so</li>
-		<li>To treat my fellow club members and our guests with respect and courtesy</li>
-		<li>To bring guests to club meetings so they can see the benefits Toastmasters membership offers</li>
-		<li>To adhere to the guidelines and rules for all Toastmasters education and recognition programs</li>
-		<li>To act within Toastmasters’ core values of integrity, respect, service and excellence during the conduct of all Toastmasters activities</li>
+		Join our club in 2 easy steps:
+		<ol>
+			<li>
+				Fill the form and pay membership fees (below)
+			</li>
+			<li>
+				Join
+				<a target="_blank" href="https://groups.google.com/forum/#!forum/mptm/join">our club's mailing list</a>.
+				This is how we send speech details and our schedule.
+			</li>
+		</ol>
 	</p>
-		<input type="checkbox" name="tm_agree_promise" required>
-		<span class="req">*</span>
-		<span class="req_mark">I agree with Toastmaster's Promise</span>
+	-->
+
+	<span class="req">*</span> fields are required
 	<br />
-</fieldset>
-<br />
 
-<fieldset>
-	<legend>Agreements</legend>
+	<form action="save.php" method="post">
 
-	<span class="req_mark">
-		<input type="checkbox" name="tm_agree_release" required>
-		<span class="req">*</span>
-		I agree with Member's Agreement and Release rules</input> (<a target="_blank" href="tm_members_agreement.html">click here to read</a>)<br />
-	</span>
+	<fieldset>
+		<legend>Personal information</legend>
 
-	<span class="req_mark">
-		<input type="checkbox" name="tm_agree_verif" required>
-		<span class="req">*</span>
-		I agree with the Verification of the Applicant</input> (<a target="popup" href="tm_verif.html">click here to read</a>)<br />
-	</span>
-</fieldset>
-<br />
+		<div>
+			<label for="firstname">First name:</label>
+			<span class="req">*</span>
+			<br />
+			<input type="text" id="firstname" name="firstname" placeholder="..." required>
+		</div>
+		<br />
 
-<input class="submit" type="submit" />
+		<div>
+			<label for="middlename">Middle name:</label>
+			<br />
+			<input type="text" id="middlename" name="middlename" placeholder="...">
+		</div>
+		<br />
 
-</form>
+		<div>
+			<label for="lastname">Last name/Surname:</label>
+			<span class="req">*</span>
+			<br />
+			<input type="text" id="lastname" name="lastname" placeholder="..." required>
+		</div>
+		<br />
 
-<script>
-let mem_type_trans_data_el = document.getElementById('membership_type_transfer_data');
-let memb_types_el = document.getElementsByClassName('membership_type');
-for (memb_type_el of memb_types_el) {
-	console.log(memb_type_el);
-	memb_type_el.addEventListener('change', function (evt) {
-		console.log(evt);
-		if (evt.srcElement.value == 'transfer') {
-			//evt.srcElement.hidden = !evt.srcElement.hidden;
-			mem_type_trans_data_el.hidden = !mem_type_trans_data_el.hidden;
-		} else {
-			mem_type_trans_data_el.hidden = true;
-		}
-	});
-}
-</script>
+
+		<div>
+			<label for="gender">Gender:</label>
+			<br />
+			<input type="radio" name="gender" value="male" />Male
+			<input type="radio" name="gender" value="female" />Female
+			<input type="radio" name="gender" value="other" checked />Other
+		</div>
+		<br />
+
+
+		<div class="lineitem">
+			<label for="address1">Address 1:</label>
+			<span class="req">*</span>
+			<br />
+			<input required type="text" id="address1" name="address1" placeholder="Address 1..." width="100%" />
+		</div>
+		<br />
+
+		<div class="lineitem">
+			<label for="address2">Address 2:</label>
+			<br />
+			<input type="text" id="address2" name="address2" placeholder="Address 2..." width="100%" />
+			<br />
+		</div>
+		<br />
+
+		<div class="lineitem">
+			<label for="zipcode">ZIP code:</label>
+			<span class="req">*</span>
+			<br />
+			<input required type="text" id="zipcode" name="zipcode" placeholder="xxxxx" size=5>
+			<br />
+		</div>
+		<br />
+
+		<div class="lineitem">
+			<label for="cityname">City:</label>
+			<span class="req">*</span>
+			<br />
+			<input required type="text" id="city" name="city" placeholder="Your city name..." size=20 />
+			<br />
+		</div>
+		<br />
+
+		<div class="lineitem">
+			<label for="state">State:</label>
+			<span class="req">*</span>
+			<br />
+			<input required type="text" id="state" name="state" placeholder="Your state..." size=20 />
+			<br />
+		</div>
+		<br />
+
+		<div class="lineitem">
+			<label for="email">E-mail:</label>
+			<span class="req">*</span>
+			<br />
+			<input required type="text" id="email" name="email" placeholder="Your e-mail..." size=20 />
+			<br />
+		</div>
+	</fieldset>
+
+
+
+
+	<fieldset width="80%">
+		<legend>Membership options:</legend>
+
+		<p>
+			<b>Are you a new member?</b>
+			<br />
+			<input type="radio" class="mptm_new_member_dues" name="mptm_new_member" value="1" checked="true" />yes ($20, initiation fee)<br/>
+			<input type="radio" class="mptm_new_member_dues" name="mptm_new_member" value="0" />no &nbsp;($0, renewal/reinstating)<br/>
+		</p>
+
+		<p>
+			<b>Month you'd like to start your membership</b><br />
+			<small>
+			(pro-rated $10/month: $7.50/month to Toastmasters International 503c non-profit and $2.50/month for running our club):
+			</small>
+			<input type="radio" class="mptm_months" name="mptm_months" value="3">January<br/>
+			<input type="radio" class="mptm_months" name="mptm_months" value="2">February<br/>
+			<input type="radio" class="mptm_months" name="mptm_months" value="1">March<br/>
+			<input type="radio" class="mptm_months" name="mptm_months" value="6">April<br/>
+			<input type="radio" class="mptm_months" name="mptm_months" value="5">May<br/>
+			<input type="radio" class="mptm_months" name="mptm_months" value="4">June<br/>
+			<input type="radio" class="mptm_months" name="mptm_months" value="3">July<br/>
+			<input type="radio" class="mptm_months" name="mptm_months" value="2">August<br/>
+			<input type="radio" class="mptm_months" name="mptm_months" value="1">September<br/>
+			<input type="radio" class="mptm_months" name="mptm_months" value="6">October<br/>
+			<input type="radio" class="mptm_months" name="mptm_months" value="5">November<br/>
+			<input type="radio" class="mptm_months" name="mptm_months" value="4">December<br/>
+		</p>
+
+		<p>
+			<b>CA sales tax</b><br />
+			<input type="radio" class="mptm_ca_tax" name="bleh" value="4" checked>$1.55<br/>
+		</p>
+	</fieldset>
+
+
+	<fieldset>
+		<legend>
+			<span class="req">*</span>
+			Membership Type
+		</legend>
+
+		<input type="radio" name="membership_type" class="membership_type" value="new" checked>New</input><br />
+		<input type="radio" name="membership_type" class="membership_type" value="reinstated">Reinstated (break in membership)</input><br />
+		<input type="radio" name="membership_type" class="membership_type" value="renewing">Renewing (no break in membership)</input><br />
+		<input type="radio" name="membership_type" class="membership_type" value="dual">Dual</input><br />
+		<input type="radio" name="membership_type" class="membership_type" value="transfer">Transfer</input><br />
+		<br />
+
+		<span id="membership_type_transfer_data" hidden>
+			<h3>Transferring: provide details here</h3>
+			Previous club name:
+			<span class="req">*</span>
+			<input type="text" name="trans_prev_club_name" placeholder="Previous club name" />
+			<br />
+
+			Previous club number:
+			<span class="req">*</span>
+			<input type="text" name="trans_prev_club_num" placeholder="Previous club number" />
+			<br />
+
+			Member number:
+			<span class="req">*</span>
+			<input type="text" name="trans_memb_number" placeholder="Member number" />
+			<br />
+		</span>
+
+
+	</fieldset>
+
+	<fieldset hidden>
+		<legend>Previous Club information</legend>
+
+		<label for="club_city">Club city:</label>
+		<input type="text" id="club_city" name="club_city" value="Menlo Park" size=30 />
+		<br />
+
+		<label for="club_name">Club name:</label>
+		<input type="text" id="club_name" name="club_name" value="Menlo Park Toastmasters" />
+
+		<label for="club_number">Club number:</label>
+		<input type="text" id="club_number" name="club_number" value="1372" />
+	</fieldset>
+
+	<fieldset>
+		<legend>
+			<span class="req">*</span>
+			A Toastmaster's Promise
+		</legend>
+
+		As a member of Toastmasters International and my club, I promise:
+		<p>
+			<li>To attend club meetings regularly</li>
+			<li>To prepare all of my projects to the best of my ability, basing them on the Toastmasters education program</li>
+			<li>To prepare for and fulfill meeting assignments</li>
+			<li>To provide fellow members with helpful, constructive evaluations</li>
+			<li>To help the club maintain the positive, friendly environment necessary for all members to learn and grow</li>
+			<li>To serve my club as an officer when called upon to do so</li>
+			<li>To treat my fellow club members and our guests with respect and courtesy</li>
+			<li>To bring guests to club meetings so they can see the benefits Toastmasters membership offers</li>
+			<li>To adhere to the guidelines and rules for all Toastmasters education and recognition programs</li>
+			<li>To act within Toastmasters’ core values of integrity, respect, service and excellence during the conduct of all Toastmasters activities</li>
+		</p>
+			<input type="checkbox" name="tm_agree_promise" required>
+			<span class="req">*</span>
+			<span class="req_mark">I agree with Toastmaster's Promise</span>
+		<br />
+	</fieldset>
+	<br />
+
+	<fieldset>
+		<legend>Agreements</legend>
+
+		<span class="req_mark">
+			<input type="checkbox" name="tm_agree_release" required>
+			<span class="req">*</span>
+			I agree with Member's Agreement and Release rules</input> (<a target="_blank" href="tm_members_agreement.html">click here to read</a>)<br />
+		</span>
+		<br />
+
+		<span class="req_mark">
+			<input type="checkbox" name="tm_agree_verif" required>
+			<span class="req">*</span>
+			I agree with the Verification of the Applicant</input> (<a target="popup" href="tm_verif.html">click here to read</a>)<br />
+		</span>
+	</fieldset>
+	<br />
+
+	<fieldset>
+		<legend>Summary</legend>
+
+		<ul>
+		<li><span id="pay_expl_link"></span>
+		</ul>
+		</p>
+
+		<p>
+		<h3 id="mptm_total_pay">Total: </h3>
+		</p>
+
+		<p>
+		<a id="pay_link" href=""></a>
+		<br/>
+		<br/>
+		<i>NOTE: You'll be redirected to the PayPal website. Credit/debit cards and PayPal are accepted.</i>
+		</p>
+	</fieldset>
+
+	<input class="submit" type="submit" />
+
+	</form>
+
+<script src="tm.js"></script>
+
+<hr />
+<small>
+	<center>
+		Copyright &copy; 2018 W. Adam Koszek and <a href="http://www.barvoy.com">Barvoy LLC</a> (contact: <a href="mailto:wkoszek@koszek.com">wojciech@koszek.com</a>)
+	</center>
+</small>
+
+</body>
+</html>
---
+++ tm.js
@@ -0,0 +1,102 @@
+function mptm_calc_dues(should_update_month) {
+	const ca_sales_tax = 1.55;
+	const mptm_monthly_fee = 2.50;
+	const tmi_monthly_fee = 7.50;
+	const new_member_fee = 20;
+
+	const cur_date = new Date();
+	const cur_month_day = cur_date.getDate();
+	const month_maybe_inc = (cur_month_day > 14) ? 1 : 0;
+	const toggle_month = (cur_date.getMonth() + month_maybe_inc) % 12;
+
+	let mptm_months_el = document.getElementsByClassName('mptm_months');
+	let mptm_new_member_dues = document.getElementsByClassName('mptm_new_member_dues');
+
+	console.assert(mptm_months_el.length == 12, "internal error: we don't have 12 months");
+	console.assert(toggle_month >= 0 && toggle_month <= 11, "we have a wrong month!");
+	if (should_update_month) {
+		mptm_months_el[toggle_month].checked = 'true';
+	}
+
+	let which_month_to_start = null;
+	let how_many_months = null;
+	let wi = 0;
+	for (let mptm_month_el of mptm_months_el) {
+		if (mptm_month_el.checked) {
+			how_many_months = parseInt(mptm_month_el.value);
+			which_month_to_start = wi;
+			break;
+		}
+		wi += 1;
+	}
+	let is_new_member = null;
+	for (let mptm_member_dues of mptm_new_member_dues) {
+		if (mptm_member_dues.checked) {
+			is_new_member = parseInt(mptm_member_dues.value);
+		}
+	}
+	console.assert(how_many_months != null, "ops1");
+	console.assert(is_new_member != null, "ops2");
+	console.assert(which_month_to_start != null, "ops3");
+
+	let to_pay = (mptm_monthly_fee + tmi_monthly_fee)*how_many_months
+			+ (new_member_fee*is_new_member)
+			+ ca_sales_tax;
+
+
+
+	let mptm_total_pay_el = document.getElementById('mptm_total_pay');
+	mptm_total_pay_el.innerHTML = "Total: $" + to_pay;
+
+	let pay_link_el = document.getElementById('pay_link');
+	pay_link_el.innerHTML = "Click HERE to pay $" + to_pay;
+	pay_link_el.href = "https://paypal.me/mptm/" + to_pay;
+
+	let tmpDate = new Date(cur_date.getFullYear(), which_month_to_start, 1);
+	let start_month_name = tmpDate.toLocaleString("en-us", { month: "long" });
+
+	let pay_expl_link_el = document.getElementById('pay_expl_link');
+	pay_expl_link_el.innerHTML = how_many_months
+		+ "-month membership in Menlo Park Toastmasters, starting "
+		+ start_month_name
+		+ " 1 ($" + (mptm_monthly_fee + tmi_monthly_fee)
+		+ "/month): $" + ((mptm_monthly_fee+tmi_monthly_fee)*how_many_months)+ "<br/>"
+		+ (is_new_member ? ("<li>New member initiation fee: $" + new_member_fee) : "")
+		+ "<li>CA sales tax: $" + (ca_sales_tax)
+		;
+
+	//console.log(how_many_months, is_new_member);
+}
+
+let r1_el = document.getElementsByClassName('mptm_new_member_dues');
+let r2_el = document.getElementsByClassName('mptm_months');
+
+for (r_el of r1_el) {
+	r_el.addEventListener('click', function() {
+		mptm_calc_dues(false);
+	});
+}
+for (r_el of r2_el) {
+	r_el.addEventListener('click', function() {
+		mptm_calc_dues(false);
+	});
+}
+
+mptm_calc_dues(true);	// call it once to update a "checked" mark near the current month
+
+
+let mem_type_trans_data_el = document.getElementById('membership_type_transfer_data');
+let memb_types_el = document.getElementsByClassName('membership_type');
+for (memb_type_el of memb_types_el) {
+	console.log(memb_type_el);
+	memb_type_el.addEventListener('change', function (evt) {
+		console.log(evt);
+		if (evt.srcElement.value == 'transfer') {
+			//evt.srcElement.hidden = !evt.srcElement.hidden;
+			mem_type_trans_data_el.hidden = !mem_type_trans_data_el.hidden;
+		} else {
+			mem_type_trans_data_el.hidden = true;
+		}
+	});
+}
+
