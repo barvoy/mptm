017cc072c6dadcd4aa13ed724455e24a2f9de727
Wojciech Adam Koszek <wojciech@koszek.com>
2018-05-13 00:05:13 -0700

    Make sure we search the checkbox.

--- app/tm.js
+++ app/tm.js
@@ -85,8 +85,10 @@ function mptm_calc_dues(should_update_month) {
 	
 	let is_new_member = null;
 	for (let el of mptm_membership_type_el) {
-		if (el.value == "new") {
+		if (el.value == "new" && el.checked) {
 			is_new_member = 1;
+		} else {
+			is_new_member = 0;
 		}
 	}
 	console.assert(how_many_months != null, "ops1");
